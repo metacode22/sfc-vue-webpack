6545472619f3456f197b886490c8e6ebed6d4313
metacode22 <alohajune22@gmail.com>
2022-12-11 17:30:52 +0900

    fix: webpack 설정 수정, 오타 수정

--- webpack.config.js
+++ webpack.config.js
@@ -23,9 +23,9 @@ module.exports = {
 	entry: './src/main.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/',
 		clean: true,
 	},
-	publicPath: '/',
 	module: {
 		rules: [
 			{
